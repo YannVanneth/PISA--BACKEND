06db9295b841583f6dbd17d45e411b4886e33f79
Vanneth Yann <yannvanneth.27@gmail.com>
2025-03-31 11:05:34 +0700

    [UPDATE] Change quantity to String type

--- database/migrations/2025_03_02_051335_ingredients.php
+++ database/migrations/2025_03_02_051335_ingredients.php
@@ -18,7 +18,7 @@ return new class extends Migration
                     ->constrained('recipes', 'recipes_id');
                 $table->string('ingredients_name_en');
                 $table->string('ingredients_name_km');
-                $table->integer('ingredients_quantity');
+                $table->string('ingredients_quantity');
                 $table->string('ingredients_unit_en');
                 $table->string('ingredients_unit_km');
                 $table->string('ingredients_image_url');
