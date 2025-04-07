bd797502696262b0c1d55be85a21b12d37748888
Vanneth Yann <yannvanneth.27@gmail.com>
2025-04-07 16:42:44 +0700

    [FIX] username

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -34,7 +34,9 @@ export interface SharedData {
 
 export interface User {
     id: number;
-    name: string;
+    // name: string;
+    first_name: string;
+    last_name: string;
     email: string;
     avatar?: string;
     email_verified_at: string | null;
